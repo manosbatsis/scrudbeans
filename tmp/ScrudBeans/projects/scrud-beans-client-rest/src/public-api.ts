1a2ce597b62f6335de17fe4a42b1a6bd4f1f4483
Manos Batsis <manosbatsis@gmail.com>
2020-07-22 08:43:57 +0300

    WiP 0.26-SNAPSHOT

--- tmp/ScrudBeans/projects/scrud-beans-client-rest/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of scrud-beans-client-rest
- */
-
-export * from './lib/scrud-beans-client-rest.service';
-export * from './lib/scrud-beans-client-rest.component';
-export * from './lib/scrud-beans-client-rest.module';
