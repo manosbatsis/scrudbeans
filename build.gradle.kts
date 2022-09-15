b16a2f5b6ba35e295e6df38d2a75e7549d7e62db
manosbatsis <manosbatsis@gmail.com>
2022-09-15 14:32:35 +0300

    chore(ci): test

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     buildsrc.convention.base
     buildsrc.convention.`kotlin-jvm`
     buildsrc.convention.`publish-jvm`
+    id("io.github.gradle-nexus.publish-plugin")
 }
 
 @Suppress("PropertyName")
@@ -11,4 +12,15 @@ version = release_version
 tasks.dokkaHtmlMultiModule.configure {
     includes.from("README.md")
     outputDirectory.set(buildDir.resolve("dokkaHtmlMultiModule"))
+}
+
+nexusPublishing {
+    repositories {
+        sonatype()
+    }
+}
+
+tasks.wrapper {
+    gradleVersion = "7.5"
+    distributionType = Wrapper.DistributionType.ALL
 }
